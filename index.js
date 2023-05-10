1d2420f2bdb3aee549d1e125ad3597e8aa06daaa
lekan <laksidesvibe@gmail.com>
2023-05-10 12:50:26 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+function submitData(userName, userEmail) {
+    const fomInfo = {name: userName, email: userEmail};
+    const handleObj = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(fomInfo)
+    };
+    return fetch("http://localhost:3000/users", handleObj)
+      .then(resp => resp.json())
+      .then(data => {
+        document.body.innerHTML += data.id;
+      })
+      .catch(error => {
+        document.body.innerHTML += error.message;
+      });
+  }
+
+  submitData("Pillbary", "pillbary@doughboy.com")
+  
